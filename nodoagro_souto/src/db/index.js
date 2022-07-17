2c6955ec5e2086aa459b3b8a31e75526b5ef1e96
Matias Souto <msouto@keepcon.com>
2022-07-16 21:48:12 -0300

    SQLite asincrónico mock

--- nodoagro_souto/src/db/index.js
+++ nodoagro_souto/src/db/index.js
@@ -2,21 +2,45 @@ import * as SQLite from 'expo-sqlite'
 
 const db = SQLite.openDatabase('address.db')
 
-export const init  = () => {
-    const promise = new Promise((resolve, reject) => {
-        db.transaction((trans) => {
-            trans.executeSql(
-                `CREATE TABLE IF NOT EXISTS address (id INTEGER PRIMARY KEY NOT NULL,
-                    title TEXT NOT NULL,
-                    image TEXT NOT NULL,
-                    address TEXT NOT NULL,
-                    coords TEXT NOT NULL)`,
-                                [],
-                () =>  {resolve()},
-                (_, err) => {reject(err)}
+export const sqlStatementMapper = async (dbInstance, sqlStatement, sqlArgs=[]) => {
+    const sqlPromise = new Promise((resolve,reject) => {
+        dbInstance.transaction((transaction) => {
+            transaction.executeSql(
+                sqlStatement,
+                sqlArgs,
+                (trans, result) => { 
+                    console.log(trans)
+                    resolve(result)
+                },
+                (trans, error) => { 
+                    console.log(trans)
+                    reject(error)}
             )
         })
     })
+    return sqlPromise
+}
+
+export const populateMock = async (user,photo,email) => {
+    return sqlStatementMapper(db,
+        'INSERT INTO profile(user_name, profile_picture , email) VALUES (?,?,?)',
+        [user,photo,email])
+
+}
+
+
+export const  init  = async () => {
+    return sqlStatementMapper(
+        db,
+        `CREATE TABLE IF NOT EXISTS profile (id INTEGER PRIMARY KEY NOT NULL,
+            user_name TEXT NOT NULL,
+            profile_picture TEXT NOT NULL,
+            email TEXT NOT NULL)`
+    )
+
+}
+
+export const select = async () => {
     
-    return promise
+    return sqlStatementMapper(db, 'SELECT * from profile')
 }
--- nodoagro_souto/src/index.js
+++ nodoagro_souto/src/index.js
@@ -1,17 +1,27 @@
+import { init, populateMock, select } from './db'
+
 import React from 'react'
 import TabNavigator from './navigation/tab-navigator/index'
 import { View } from 'react-native'
-import { init } from './db'
 import { styles } from './styles'
 
 init()
   .then(() => {
     console.log("DB IS UP")
+    console.log()
   })
   .catch((err) => {
     console.log(err)
   })
+populateMock("usuario1","foto1","email1")
+populateMock("usuario2","foto2","email2")
+populateMock("usuario3","foto3","email3")
 
+select()
+  .then((res) => {
+    console.log("USUARIOS")
+    console.log(res)
+  })
 
 const Root = () => {
   return (
